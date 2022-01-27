6043ee8b239a6c16f2cb5e3a1eec03f4c2583998
StefanStoynov <sstoynov87@gmail.com>
2022-01-27 19:59:21 +0200

    Initial commit

---
+++ src/main/java/com/ss/datademojdbctojpa/DataDemoJdbcToJpaApplication.java
@@ -0,0 +1,13 @@
+package com.ss.datademojdbctojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDemoJdbcToJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataDemoJdbcToJpaApplication.class, args);
+    }
+
+}
